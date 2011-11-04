95194fa3381e29b54de48e7907f9c0f864528a8c
Duane <thdunston@gmail.com>
2011-11-04 16:28:41 -0400

    - Starting on Lesson 8. Yay!

---
+++ 05identification/curriculum.php
@@ -0,0 +1,123 @@
+<?php
+
+/*session_start();
+
+if ($_SESSION['t_license'] != "user_agreed") {
+
+	echo "Sorry you must agree to the license first";
+	exit();
+
+}*/
+
+?>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xml:lang="en" xmlns="http://www.w3.org/1999/xhtml"><head>
+
+<title>Survivability and Information Assurance Curriculum-</title>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<link rel="stylesheet" href="css/1.css" type="text/css" media="screen,projection">
+
+</head><body>
+
+<div id="wrapper">
+
+		<div id="header">
+		
+				<p class="description">
+				
+						Originally developed by the <a href="http://www.cert.org/sia/">Carnegie Mellon CERT</a><br /> and revitalized with their permission.
+				
+				</p>
+				
+				<h1><a href="#">Survivability and Information<br /><br /> Assurance Curriculum</a></h1>
+				
+		</div>
+		
+		<div id="sidebar">
+		
+				<h2>Latest News</h2>
+				
+				<p class="news">
+				
+				</p>
+				<h2>Sub Navigation</h2>
+				
+				<ul>
+				
+						<li><a href="index.html#overFoundation">SIA Curriculum Foundations</a></li>
+						
+						<li><a href="index.html#overCurriculum">SIA Curriculum Overview</a></li>
+						
+						<li><a href="index.html#overLab">SIA Lab Overview</a></li>
+						
+						<li><a href="index.html#volunteer">Downloading the Curriculum</a></li>
+
+						<li><a href="index.html#volunteers">Volunteers</a></li>
+				
+				</ul>
+				
+		</div>
+		
+		<div id="content">
+		
+				<h2><a href="#">Downloads</a></h2>
+				
+				<p>
+				
+					<ul>
+
+					<li><a href="sia-docs/CoverPage_student.doc">Coverpage Warranty</a> - 08/21/2011</li>
+					<li>Principle 00 - Introduction<br />
+
+					<b>* Reading Material:</b><br />
+					- <a href="sia-docs/00introduction.zip">Contains all documents and formats.</a> - 08/21/2011<br />
+					</li>
+					<li>Principle 01 - Survivability<br />
+					<b>* Reading Material:</b><br />
+					- <a href="sia-docs/01survive.zip">Contains all documents and formats.</a> - 08/21/2011<br />
+					</li>
+
+					<li>Principle 02 - Everything is Data<br />
+					<b>* Reading Material:</b><br />
+					- <a href="sia-docs/02everything.zip">Contains all documents and formats.</a> - 08/21/2011<br />
+					</li>
+
+					<li>Principle 03 - Risk Management<br />
+					<b>* Reading Material:</b><br />
+					- <a href="sia-docs/03risk.zip">Contains all documents and formats.</a> - 08/21/2011<br />
+					</li>
+
+					<li>Principle 04 - Policy<br />
+					<b>* Reading Material:</b><br />
+					- <a href="sia-docs/04policy.zip">Contains all documents and formats.</a> - 08/21/2011<br />
+					</li>
+
+					<li>Principle 05 - Identification<br />
+					<b>* Reading Material:</b><br />
+					- <a href="sia-docs/05identification.zip">Contains all documents and formats.</a> - 08/22/2011<br />
+					</li>
+					<li>Principle 06 - SFUs</li>
+					<li>Principle 07 - SKiP</li>
+					<li>Principle 08 - Roadmap</li>
+					<li>Principle 09 - Challenge Assumptions</li>
+					<li>Principle 10 - Communication</li>
+					<li>Principle 10 - Communication-Cost-Return</li>
+					</ul>
+
+		</div>
+		
+		<div id="footer">
+		
+								<!-- Please leave this line intact -->
+								<p>Template design by <a href="http://www.sixshootermedia.com/">Six Shooter Media</a>.<br>
+								<!-- you can delete below here -->
+
+								© Learnsia.org.</p>
+		
+		</div>
+
+</div>
+
+
+</body></html>
